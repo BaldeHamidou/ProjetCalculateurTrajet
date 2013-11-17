726530fe4e3a431b7e76be46425d238635ae8b88
Charly <charlyfarot@gmail.com>
2013-11-17 19:08:58 +0100

    suite ajout fichier csv ligne simple

--- src/Application.java
+++ src/Application.java
@@ -4,11 +4,11 @@ import calculateur.implementations.MetroReseau;
 
 public class Application {
 	
-	private final static String LISTE_METRO = "/MetroReseau/ListeMetros.csv";
+	private final static String LISTE_STATIONS = "/MetroReseau/ListeStations.csv";
 	
 	public static void main(String[] args) throws IOException {
 		
-		MetroReseau mr = new MetroReseau(CsvFileHelper.getResource(LISTE_METRO));
+		MetroReseau mr = new MetroReseau(CsvFileHelper.getResource(LISTE_STATIONS));
 		mr.loadReseauFromCSV();		
 		
 		//TEST sur la première station!
